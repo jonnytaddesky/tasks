7cd77a63731aa840e98f0aec51db7ed011a951ee
jonnytaddesky <s.v.artem2020@gmail.com>
2021-08-31 23:24:49 +0300

    convert second to day:hour:minute:second

---
+++ task_9.py
@@ -0,0 +1,14 @@
+seconds = 4545345
+
+
+def calculate(seconds):
+    days = seconds // (24 * 3600)
+    seconds %= 24 * 3600
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    print(f'{days}:{hours}:{minutes}:{seconds}')
+
+
+calculate(seconds)
